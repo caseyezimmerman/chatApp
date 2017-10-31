f739ac8924b4af2d732d757af0c2004e34f70566
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-10-31 16:11:23 -0400

    third commit

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,15 @@ $(document).ready(()=>{
 	// console.log(io);
 	var socketio = io.connect(socketUrl);
 	var name = prompt("what is your name?")
+	var today = new Date();
+	var year = today.getFullYear();
+	var month = today.getMonth()+1;
+	var day = today.getDate();
+	var hour = today.getHours()
+	var minute = today.getMinutes()
+	var second = today.getSeconds()
+	today = year + '/' + month + '/' + day + "  " + hour +':'+minute+':'+second;
+
 	//take the users name and send it to the server
 	//emit takes 2 args
 	// 1. event (we make this up)
@@ -17,8 +26,12 @@ $(document).ready(()=>{
 		// console.log(`${userName} just joined`)
 		// $('#users').append(`<div class="col-sm-12">${userName}</div>`)
 		var usersHTML = ""
+		usersHTML = `<h4>Online Users</h4>`;
 		users.map((user)=>{
-			usersHTML+= `<div class="col-sm-12">${user.name}</div>`
+			usersHTML+= `<div class="col-sm-12 bold">${user.name}</div>`;
+			// usersHTML+=`<form id="logout">`
+			usersHTML+=`<button type="submit" class="btn btn-muted">Logout</button>`
+			// usersHTML+=`</form>`
 		});
 		$('#users').html(usersHTML) //overwiting this div with the string above
 	});
@@ -30,12 +43,23 @@ $(document).ready(()=>{
 		//get value from input box
 		var newMessage = $('#new-message').val();
 		///user socketio, to send data to the server
-		socketio.emit('MessageToServer',{
+		socketio.emit('messageToServer',{
+			today: today,
 			name: name,
 			message: newMessage
 		});
+		$("#new-message").val("");
 	});
 	socketio.on('messageToClient',(messageObject)=>{
-		$('#messages').prepend(`<p>${messageObject.message}--${messageObject.name}</p>`)
+		$('#messages').prepend(`<span>${messageObject.today}:</span> <span class="fancy">${messageObject.name} </span> <span>${messageObject.message}</span><br/>`)
 	})
+	
 });
+
+// 	$('#logout').submit((event)=>{
+// 		event.preventDefault();
+// 		console.log('click')
+// 		$('users').splice(user.name, 1);
+// 	})
+// });
+
